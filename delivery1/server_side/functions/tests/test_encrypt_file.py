7c87578a987ea4379e6c9df1d879aae5750cd504
Caroline Ribeiro <108082352+CarolineRibeiro19@users.noreply.github.com>
2025-01-29 14:22:28 +0000

    Delete delivery1 directory

--- delivery1/server_side/functions/tests/test_encrypt_file.py
+++
@@ -1,8 +0,0 @@
-from cipher import encrypt_file
-
-key = bytes.fromhex('0700d603a1c514e46b6191ba430a3a0c')
-iv = bytes.fromhex('aad1583cd91365e3bb2f0c3430d065bb')
-
-encrypt_file('../files/example.txt', key, iv)
-
-#encrypt_file('files/exemplo.pdf', key, iv)
